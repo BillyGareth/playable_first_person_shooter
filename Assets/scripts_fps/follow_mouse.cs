c3e9f76f0b720f4d3d5c896f1677feb08ecfbe1d
BillyGareth <billygareth01@gmail.com>
2020-02-28 02:24:21 +0300

    initial commit

---
+++ Assets/scripts_fps/follow_mouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_mouse : MonoBehaviour
+{
+    private TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            trail.startColor = Color.green;
+            trail.endColor = Color.yellow;
+        }
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos;
+    }
+}
